59dca226ad17ea256b6dcbda60b8fcdbede1c1eb
Reinaldo Almeida de Jesus <reinaldojesus238@gmail.com>
2025-12-21 22:05:54 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Department dept = new Department(1, "Books");
+		
+		Seller seller = new Seller(1, "Bob Brown", " bob@gmail.com", new java.util.Date(), 3000.0, dept);
+		
+		System.out.println(seller);
+		
 		System.out.println(dept);
 	}
 
